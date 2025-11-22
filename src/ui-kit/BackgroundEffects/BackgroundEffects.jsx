c6be68ad95d2f7ad5148f859b85a3967df5d9923
scott <st.scott0612@gmail.com>
2025-11-22 16:07:36 +0000

    using new bg video

--- src/ui-kit/BackgroundEffects/BackgroundEffects.jsx
+++ src/ui-kit/BackgroundEffects/BackgroundEffects.jsx
@@ -2,7 +2,17 @@ import React from 'react';
 
 const BackgroundEffects = () => {
   return (
-    <></>
+    <video
+      className="bg-video"
+      autoPlay
+      muted
+      loop
+      playsInline
+      preload="auto"
+      aria-hidden="true"
+    >
+      <source src="/static_assets/bg_video.webm" type="video/webm" />
+    </video>
   );
 };
 
